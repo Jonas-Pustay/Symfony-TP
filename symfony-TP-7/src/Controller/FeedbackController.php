120eb60a9df3a0b9f2ffd845ff26aa8eeb352a64
Jonas-Pustay <jonas.pustay@outlook.fr>
2024-01-10 16:58:04 +0100

    Finish

--- symfony-TP-7/src/Controller/FeedbackController.php
+++ symfony-TP-7/src/Controller/FeedbackController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Entity\Feedback;
 use App\Form\FeedbackType;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,31 +18,49 @@ class FeedbackController extends AbstractController
     #[Route('/ShowFeedback', name: 'Feedback_list')]
     public function showFeedback(ManagerRegistry $doctrine): Response
     {
-        $entityManager = $doctrine->getManager();
-        
-        $obj1 = (object)['name' => 'Casque', 'price' => 42,];
-        $obj2 = (object)['name' => 'Souris', 'price' => 23,];
-        $obj3 = (object)['name' => 'Téléphone', 'price' => 200,];
+        $repository = $doctrine->getRepository(Feedback::class);
+        $Feedbacks = $repository->findAll();
 
-        $products = [$obj1, $obj2, $obj3];
-        return $this->render('feedback\showFeedback.html.twig', ['controller_name' => "ShowFeedback", 'feedbacks' => $products]);
+        return $this->render('feedback\showFeedback.html.twig', ['controller_name' => "ShowFeedback", 'feedbacks' => $Feedbacks]);
     }
 
     #[Route('/EditFeedback/{id<\d+>}', name: 'EditFeedback')]
-    public function EditFeedback($id, Request $request): Response
+    public function EditFeedback($id, Request $request, ManagerRegistry $doctrine): Response
     {
-        $Feedback = new Feedback();
+        $FeedbackID = $id;
+
+        $entityManager = $doctrine->getManager();
+        $Feedback = $entityManager->getRepository(Feedback::class)->find($FeedbackID);
+
         $FeedbackForm = $this->createForm(FeedbackType::class, $Feedback);
         $FeedbackForm->handleRequest($request);
         if ($FeedbackForm->isSubmitted() && $FeedbackForm->isValid())
         {
-            // Traitement des données soumises, par exemple, enregistrement dans la base de données.
-        // Pour l'instant, affichons simplement les données soumises :
-            dump($request->request->all());
+            $entityManager = $doctrine->getManager();
+            $client = $FeedbackForm->getData();
+            $entityManager->persist($client);
+            $entityManager->flush();
             
-            return $this->render('feedback/SaveFeedback.html.twig', []);
+            return $this->render('feedback/SaveFeedback.html.twig', [ 'feedbackid' => $FeedbackID ]);
         }
 
-        return $this->render('feedback/EditFeedback.html.twig', [ 'feedbackform' => $FeedbackForm->createView(), ]);
+        return $this->render('feedback/EditFeedback.html.twig', [ 'feedbackform' => $FeedbackForm->createView(), 'feedbackid' => $FeedbackID ]);
     }
+
+        #[Route('/RemoveFeedback/{id<\d+>}', name: 'Feedback_remove')]
+        public function RemoveFeedback($id, ManagerRegistry $doctrine): Response
+        {
+            $FeedbackID = $id;
+
+            $entityManager = $doctrine->getManager();
+            $Feedback = $entityManager->getRepository(Feedback::class)->find($FeedbackID);
+            if (!$Feedback)
+            {
+                throw $this->createNotFoundException('No customer found for id '.$FeedbackID);
+            }
+            $entityManager->remove($Feedback);
+            $entityManager->flush();
+
+            return $this->render('feedback\RemoveFeedback.html.twig', [ 'feedbackid' => $FeedbackID ]);
+        }
 }
